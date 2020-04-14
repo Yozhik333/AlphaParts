0a3391f5884cf32ddd0e9739fe0734fe12a88f24
Yozhik333 <repeinik333@gmail.com>
2020-04-14 17:42:53 +0600

    Catalog, six-section

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-	var logo = document.getElementById('logo');
+	let logo = document.querySelector('.logo');
 
 	for (let i = 1; i <= 5; i++) { // write var and check
-		var item = document.createElement('span');
+		let item = document.createElement('span');
 
 		logo.appendChild(item);
 	}
